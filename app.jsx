defb8e77ee97cd3988761a00d20572d7ee4acb78
nihalxz <zxmhdnihal@gmail.com>
2026-08-11 23:04:04 +0530

    Change success sound to a more professional chime

--- app.jsx
+++ app.jsx
@@ -507,7 +507,7 @@ const App = () => {
                 setIsSubmitting(false);
 
                 try {
-                    const audio = new Audio('https://assets.mixkit.co/active_storage/sfx/2013/2013-preview.mp3');
+                    const audio = new Audio('https://assets.mixkit.co/active_storage/sfx/2869/2869-preview.mp3');
                     audio.play().catch(e => console.log("Audio error: ", e));
                 } catch(e) {}
 
